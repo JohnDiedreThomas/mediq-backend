e35c296370791f71844f07560c64cf73d8dc346c
JohnDiedreThomas <thomasjohndiedre@gmail.com>
2026-02-22 22:57:29 -0800

    fix email transporter

--- utils/email.js
+++ utils/email.js
@@ -1,24 +1,27 @@
-const { Resend } = require("resend");
+const nodemailer = require("nodemailer");
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
-const sendEmail = async (to, subject, text) => {
-  try {
-    await transporter.sendMail({
-      from: `"MediQ Clinic" <${process.env.EMAIL_USER}>`,
-      to,
-      subject,
-      text,
-      html: `
-        <p>Click below to reset your password:</p>
-        <a href="${text}">Reset Password</a>
-      `,
-    });
-
-    console.log("Email sent to:", to);
-  } catch (err) {
-    console.error("EMAIL SEND ERROR:", err);
-  }
+const sendEmail = async (to, subject, link) => {
+  await transporter.sendMail({
+    from: `"MediQ Clinic" <${process.env.EMAIL_USER}>`,
+    to,
+    subject,
+    html: `
+      <p>You requested a password reset.</p>
+      <p>Click below:</p>
+      <a href="${link}" style="background:#2F80ED;color:white;padding:10px 20px;border-radius:6px;text-decoration:none;">
+        Reset Password
+      </a>
+      <p>If you didn’t request this, ignore this email.</p>
+    `,
+  });
 };
 
 module.exports = sendEmail;
