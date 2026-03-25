a380c6a057d9b2b4a21330fa629df80d8888168d
JohnDiedreThomas <thomasjohndiedre@gmail.com>
2026-03-25 10:22:46 -0800

    debug update route

--- routes/admin.services.js
+++ routes/admin.services.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const db = require("../db");
 const adminAuth = require("../middleware/adminAuth");
-
 const upload = require("../middleware/uploadServiceCloudinary");
 
 /* 🔒 Protect all admin service routes */
